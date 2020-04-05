55e34a80e2bf66c4c94883bafb447c0e5ca9b005
luik-2210 <56349657+luik-2210@users.noreply.github.com>
2020-04-06 01:17:34 +0530

    created positivenos.py
    this is a python file which prints only positve numbers

---
+++ positive_nos.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list=[int(x) for x in input().split()]
+a=[]
+for i in list:
+    if i>=0:
+        a.append(i)
+print(a)
+
+
+# In[ ]:
+
+
+
+
